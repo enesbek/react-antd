c458b02614117e466d9067df7b9b7f929d79345c
Enes Bek <enesbek@Enes-MacBook-Air.local>
2022-12-16 11:14:11 +0300

    feat: home page tables created

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,47 @@
 import React from "react";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+import { CompanyTable } from "./home/CompanyTable";
+import { LastAddedCompanies } from "./home/LastAddedCompanies";
+import { CarouselCompany } from "./home/CarouselCompany";
+
+const { Header, Content, Footer } = Layout;
+
+const navItems: MenuProps["items"] = ["Home", "Companies", "Products"].map(
+  (key) => ({
+    key,
+    label: `${key}`,
+  })
+);
 
 export const Home = () => {
-  return <div>Home</div>;
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["Home"]}
+          items={navItems}
+        />
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <Layout style={{ padding: "24px 0", background: colorBgContainer }}>
+          <Content style={{ padding: "0 24px", minHeight: 280 }}>
+            <CompanyTable />
+            <LastAddedCompanies />
+            <CarouselCompany />
+          </Content>
+        </Layout>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
 };

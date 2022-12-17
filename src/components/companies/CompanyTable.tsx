23ae9a588ea93dd4cf6d317027596489234ed0bf
Enes Bek <enesbek@Enes-MacBook-Air.local>
2022-12-17 14:32:50 +0300

    feat: add company table edit, delete, add features

--- src/components/companies/CompanyTable.tsx
+++ src/components/companies/CompanyTable.tsx
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
-import { Form, Input, InputNumber, Popconfirm, Table, Typography } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Popconfirm,
+  Table,
+  Typography,
+} from "antd";
 import CompanyData from "../../companyData.json";
+import Item from "antd/es/list/Item";
 
 interface Item {
   Id: number;
@@ -57,44 +66,75 @@ const EditableCell: React.FC<EditableCellProps> = ({
   );
 };
 
-const App: React.FC = () => {
+const CompaniesTable: React.FC = () => {
   const [form] = Form.useForm();
   const [data, setData] = useState(originData);
   const [editingKey, setEditingKey] = useState(0);
+  const [deletingKey, setDeletingKey] = useState(0);
+
+  useEffect(() => {}, [data]);
 
   const isEditing = (record: Item) => record.Id === editingKey;
+  const isDeleting = (record: Item) => record.Id === deletingKey;
 
   const edit = (record: Partial<Item> & { Id: number }) => {
-    form.setFieldsValue({ name: "", age: "", address: "", ...record });
+    form.setFieldsValue({
+      Company_Name: "",
+      Legal_Number: "",
+      Country: "",
+      User: "",
+      Website: "",
+      ...record,
+    });
     setEditingKey(record.Id);
+  };
+
+  const deleteItem = (record: Partial<Item> & { Id: number }) => {
+    setDeletingKey(record.Id);
   };
 
   const cancel = () => {
     setEditingKey(0);
   };
 
-  const save = async (key: React.Key) => {
-    try {
-      const row = (await form.validateFields()) as Item;
+  const deleteConfirm = () => {
+    var deletingIndex = 0;
+    const newData = [...data];
 
-      const newData = [...data];
-      const index = newData.findIndex((item) => key === item.Id);
-      if (index > -1) {
-        const item = newData[index];
-        newData.splice(index, 1, {
-          ...item,
-          ...row,
-        });
-        setData(newData);
-        setEditingKey(0);
-      } else {
-        newData.push(row);
-        setData(newData);
-        setEditingKey(0);
-      }
-    } catch (errInfo) {
-      console.log("Validate Failed:", errInfo);
-    }
+    newData.forEach((company, index) => {
+      if (company.Id === deletingKey) deletingIndex = index;
+    });
+
+    newData.splice(deletingIndex, 1);
+    CompanyData.splice(deletingIndex, 1);
+
+    setData(newData);
+    setDeletingKey(0);
+  };
+
+  const save = async (key: React.Key) => {
+    const row = (await form.validateFields()) as Item;
+
+    const newData = [...data];
+    const index = newData.findIndex((item) => key === item.Id);
+    const item = newData[index];
+
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+
+    CompanyData[index] = newData[index];
+
+    setData(newData);
+    setEditingKey(0);
+  };
+
+  const addNewCompany = () => {
+    const newData = [...data];
+    const lastId = CompanyData[CompanyData.length - 1].Id + 1;
+    newData.push({ Id: lastId, ...{} } as Item);
+    setData(newData);
   };
 
   const columns = [
@@ -152,23 +192,17 @@ const App: React.FC = () => {
     {
       title: "Delete",
       render: (_: any, record: Item) => {
-        const editable = isEditing(record);
+        const editable = isDeleting(record);
         return editable ? (
           <span>
-            <Typography.Link
-              onClick={() => save(record.Id)}
-              style={{ marginRight: 8 }}
-            >
-              Save
-            </Typography.Link>
-            <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
-              <a>Cancel</a>
+            <Popconfirm title="Sure to delete?" onConfirm={deleteConfirm}>
+              <a style={{ color: "red" }}>Delete</a>
             </Popconfirm>
           </span>
         ) : (
           <Typography.Link
-            disabled={editingKey !== 0}
-            onClick={() => edit(record)}
+            disabled={deletingKey !== 0}
+            onClick={() => deleteItem(record)}
           >
             Delete
           </Typography.Link>
@@ -209,8 +243,11 @@ const App: React.FC = () => {
           onChange: cancel,
         }}
       />
+      <Button type="primary" onClick={addNewCompany}>
+        Add New Company
+      </Button>
     </Form>
   );
 };
 
-export default App;
+export default CompaniesTable;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import companySlice from "../features/companySlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    companies: companySlice
+  },
 });
 
 export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const UseAppDispatch = () => useDispatch<AppDispatch>();
+export const UseAppSelector: TypedUseSelectorHook<RootState> = useSelector;
